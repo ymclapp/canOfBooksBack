3dafd6d602e35840a2cde8599a6694f813a82eae
ymcla <ymclapp@yahoo.com>
2021-09-16 11:12:17 -0500

    can't figure out how to get books to show

--- seed.js
+++ seed.js
@@ -3,33 +3,38 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URL);
 
-const Books = require('./models/Book');
+const Books = require('./models/bookRoute');  //constructor
 
 async function seed() {
     //Deleting all of my books and starting over
     console.log('Deleting existing books')
     await Books.deleteMany({});
 
-    const myBook = new Books({
+    const myBook1 = new Books({
         title:  "Where the Red Fern Grows",
         description: "A sad book",
         status: "Out",
         email: "ymclapp@yahoo.com",
-        },
-        {
+        });
+        await  myBook1.save();
+
+
+    const myBook2 = new Books({
         title:  "Discovery of Witches",
         description: "A witch book",
         status: "In",
         email: "ymclapp@yahoo.com",
-        },
-        {
+        });
+        await  myBook2.save();
+        
+    const myBook3 = new Books({
         title:  "Neon Prey",
         description: "A Lucas Davenport book",
         status: "In",
         email: "ymclapp@yahoo.com",
-        })
+        });
+        await myBook3.save();
 
-    myBook.save();
     mongoose.disconnect();
 }
 
